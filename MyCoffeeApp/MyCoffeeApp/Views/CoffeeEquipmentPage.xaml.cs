ea1a2460efc1484316eceb8e64b61b6cece3df24
Duncan Summers <djsummers36@gmail.com>
2021-11-01 11:37:09 -0400

    first commit

---
+++ MyCoffeeApp/MyCoffeeApp/Views/CoffeeEquipmentPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyCoffeeApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CoffeeEquipmentPage : ContentPage
+    {
+        public CoffeeEquipmentPage()
+        {
+            InitializeComponent();
+            IncreaseCount = new Command(OnIncrease);
+            BindingContext = this;
+        }
+
+        public ICommand IncreaseCount { get; }
+        
+        int count = 0;
+        string countDisplay = "Click Me";
+
+
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set
+            {
+                if (value == countDisplay)
+                    return;
+
+                countDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+        void OnIncrease()
+        {
+                        count++;
+            CountDisplay = $"You clicked {count} time(s)";
+        }
+
+    }
+}
